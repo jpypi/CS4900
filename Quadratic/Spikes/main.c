88e02e80ef5da2ba54e651c8434d1981935003fa
James Jenkins <applesmacs@yahoo.com>
2016-09-19 11:33:33 -0400

    Start naive quad solver

--- Quadratic/Spikes/main.c
+++ Quadratic/Spikes/main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <math.h>
+
+// -b +- sqrt(pow(b, 2) - 4*a*c) / 2*a
+void
+solve(float a, float b, float c) {
+    float b_sq = powf(b, 2);
+    float intermediate = 4*a*c;
+    float descriminate = sqrtf(b_sq - intermediate);
+    float root1 = (-b + descriminate) / 2*a;
+    float root2 = (-b - descriminate) / 2*a;
+
+    printf("Root 1: %f\n", root1);
+    printf("Root 2: %f\n", root2);
+}
 
 int
 main(int argc, char *argv[]) {
+    solve(1.0, 2.0, 4.0);
     // TODO: Get input
-    // TODO: Solve quadratic equation
     // TODO: Give output
     return 0;
 }
