748989c0c6a50f5bf7d5601e5ccd7f0134030850
James Jenkins <applesmacs@yahoo.com>
2016-10-26 11:04:05 -0400

    Split all ops into single math ops; func proto update

--- Quadratic/spikes/naive_solver/main.c
+++ Quadratic/spikes/naive_solver/main.c
@@ -2,21 +2,30 @@
 #include <math.h>
 
 // -b +- sqrt(pow(b, 2) - 4*a*c) / 2*a
-void
-solve(float a, float b, float c) {
-    float b_sq = powf(b, 2);
-    float intermediate = 4*a*c;
-    float descriminate = sqrtf(b_sq - intermediate);
-    float root1 = (-b + descriminate) / 2*a;
-    float root2 = (-b - descriminate) / 2*a;
+// ax^2 + b*x + c
+int solve(double a, double b, double c, double *root1, double *root2) {
+    double b_sq = powf(b, 2);
+    double four_a = 4 * a;
+    double four_a_c = four_a * c;
+    double descriminate_pre = b_sq - four_a_c;
+    double descriminate = sqrtf(descriminate_pre);
+    double two_a = 2 * a;
+    double numerator_1 = -b + descriminate;
+    double numerator_2 = -b - descriminate;
+    *root1 = numerator_1 / two_a;
+    *root2 = numerator_2 / two_a;
 
-    printf("Root 1: %f\n", root1);
-    printf("Root 2: %f\n", root2);
+    return 0;
 }
 
 int
 main(int argc, char *argv[]) {
-    solve(1.0, 2.0, 4.0);
+    double r1, r2;
+    solve(1.0, 2.0, 1.0, &r1, &r2);
+
+    printf("Root 1: %g\n", r1);
+    printf("Root 2: %g\n", r2);
+
     // TODO: Get input
     // TODO: Give output
     return 0;
