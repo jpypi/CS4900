a477db75b3e515ad371fbff1b2cc1a0db2e6c45e
James Jenkins <applesmacs@yahoo.com>
2016-11-07 11:24:01 -0500

    Make make hierarchy work.

---
+++ Quadratic/src/output/output.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void request_input() {
+    printf("Please input: a b c> ");
+}
---
+++ Quadratic/src/qs/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "input.h"
+#include "output.h"
+#include "qsolve.h"
+
+int main(int argc, char *argv[]) {
+    // TODO: Get input
+    // TODO: Call qsolve
+    // TODO: Write output
+    return 0;
+}
