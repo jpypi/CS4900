afb56d7d6185a034570fb3e2106cbdba1c45e102
James Jenkins <applesmacs@yahoo.com>
2016-11-12 18:15:43 -0500

    Add some output printing code (single/double root)

--- Quadratic/src/output/output.c
+++ Quadratic/src/output/output.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <float.h>
 
-void request_input() {
-    printf("Please input: a b c> ");
+
+char * double_root_str(double root) {
+    printf("Double root.\n");
+    printf("Root: %.*le\n", FLT_DIG, root);
+    return NULL;
+}
+
+char * single_root_str(double root1, double root2) {
+    printf("Root 1: \nRoot 2: \n");
+    return NULL;
 }
---
+++ Quadratic/src/output/output.h
@@ -0,0 +1,7 @@
+#ifndef _QSOLVE_OUTPUT
+#define _QSOLVE_OUTPUT
+
+char * double_root_str(double root);
+char * single_root_str(double root);
+
+#endif
