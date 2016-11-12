e0b5d6f36d44a3712d8374899502711dafa00351
James Jenkins <applesmacs@yahoo.com>
2016-11-12 17:04:16 -0500

    Add define return values for qsolve

--- Quadratic/src/qsolve/qsolve.c
+++ Quadratic/src/qsolve/qsolve.c
@@ -1,4 +1,9 @@
-int
-qsolve(double a, double b, double c, double *r1, double *r2) {
-    return 0;
+#include "qsolve.h."
+/*
+ * Return values:
+ *  QSOLVE_X
+ *  See qsolve.h for possible values
+ */
+int qsolve(double a, double b, double c, double *r1, double *r2) {
+    return QSOLVE_OK;
 }
---
+++ Quadratic/src/qsolve/qsolve.h
@@ -0,0 +1,11 @@
+#ifndef _QSOLVE
+#define _QSOLVE
+
+#define QSOLVE_IMG_ROOTS -1
+#define QSOLVE_OK 0
+
+/*
+ */
+int qsolve(double a, double b, double c, double *r1, double *r2);
+
+#endif
