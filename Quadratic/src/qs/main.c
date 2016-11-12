54d33de1132d69bac23cef79dcdd9f008f0452b7
James Jenkins <applesmacs@yahoo.com>
2016-11-12 18:19:26 -0500

    Make use of output code in main

--- Quadratic/src/qs/main.c
+++ Quadratic/src/qs/main.c
@@ -12,20 +12,19 @@ int main(int argc, char *argv[]) {
     get_coeficient("Input b: ", &b);
     get_coeficient("Input c: ", &c);
 
-    printf("%le %le %le\n", a, b, c);
-
     double root1, root2;
     int res = qsolve(a,b,c, &root1, &root2);
 
     char *output = NULL;
 
     if (res == QSOLVE_DOUBLE_ROOT) {
-        printf("Wee");
+        output = single_root_str(root1, root2);
     } else if (res == QSOLVE_SINGLE_ROOT) {
-        printf("Weewooo");
+        output = double_root_str(root1);
+    } else {
+        output = "Well, y' dun goof'd\n";
+        printf(output);
     }
-
-    //printf(output);
 
     return 0;
 }
