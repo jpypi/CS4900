4e25209c0cc93aca8a484388f2bcc64f11adddba
James Jenkins <applesmacs@yahoo.com>
2016-11-12 18:39:49 -0500

    Order root output smallest first

--- Quadratic/src/output/output.c
+++ Quadratic/src/output/output.c
@@ -9,6 +9,12 @@ char * double_root_str(double root) {
 }
 
 char * single_root_str(double root1, double root2) {
-    printf("Root 1: \nRoot 2: \n");
+    double first = root1, second = root2;
+    if (root2 < root1) {
+        first = root2;
+        second = root1;
+    }
+
+    printf("Root 1: %.*le\nRoot 2: %.*le\n", FLT_DIG, first, FLT_DIG, second);
     return NULL;
 }
